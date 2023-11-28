c75e3f819ea514fb750c33abaa1f941b93da53b1
Matrix30 <Matrixboy30@gmail.com>
2023-11-28 15:16:06 +0100

    finish task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -16,6 +16,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 
+	while (parent->left)
+	{
+		parent = parent->left;
+	}
+
 	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
 	{
