1948677fee5d4727f2f7860e600b39c455607143
Matrix30 <Matrixboy30@gmail.com>
2023-11-29 17:45:03 +0100

    finish task 14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree to measure the balance factor
+ * Return: the balance factor
+*/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left_height = 0, right_height = 0;
+	binary_tree_t *tmp;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	tmp = tree->left;
+	while (tmp != NULL)
+	{
+		left_height += 1;
+		tmp = tmp->left;
+	}
+	tmp = tree->right;
+	while (tmp != NULL)
+	{
+		right_height += 1;
+		tmp = tmp->right;
+	}
+	return (left_height - right_height);
+}
