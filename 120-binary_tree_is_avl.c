f3a8b7dd0640ad785fe9c954444c48aa9315b130
Matrix30 <Matrixboy30@gmail.com>
2023-12-02 03:39:19 +0100

    finish 120

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -15,45 +15,75 @@ int maximum(int a, int b)
 	return (b);
 }
 
-int height(const binary_tree_t *tree) {
-    if (tree == NULL) {
-        return 0;
-    }
-    return 1 + maximum(height(tree->left), height(tree->right));
+/**
+ * height- returns height of tree
+ * @tree: pointer to root node
+ * Return: the height of tree
+*/
+int height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	return (1 + maximum(height(tree->left), height(tree->right)));
 }
 
-int is_valid_avl_helper(const binary_tree_t *node) {
-    if (node == NULL) {
-        return 1;
-    }
+/**
+ * valid_avl - checks for validity of avl
+ * @node: pointer to root of tree
+ * Return: height of tree
+*/
+int valid_avl(const binary_tree_t *node)
+{
+	int left_height, right_height;
 
-    int left_height = height(node->left);
-    int right_height = height(node->right);
+	if (node == NULL)
+	{
+		return (1);
+	}
 
-    if (abs(left_height - right_height) > 1) {
-        return 0;
-    }
+	left_height = height(node->left);
+	right_height = height(node->right);
 
-    return is_valid_avl_helper(node->left) && is_valid_avl_helper(node->right);
+	if (abs(left_height - right_height) > 1)
+	{
+		return (0);
+	}
+
+	return (valid_avl(node->left) && valid_avl(node->right));
 }
 
-int binary_tree_is_bst_helper(const binary_tree_t *node, int min, int max) {
-    if (node == NULL) {
-        return 1;
-    }
-    if (node->n <= min || node->n >= max) {
-        return 0;
-    }
-    return binary_tree_is_bst_helper(node->left, min, node->n) && 
-           binary_tree_is_bst_helper(node->right, node->n, max);
+/**
+ * is_bst - checks for validity of avl
+ * @node: pointer to root of tree
+ * @min: minimum
+ * @max: maximum
+ * Return: 1 if valid 0 otherwise
+*/
+int is_bst(const binary_tree_t *node, int min, int max)
+{
+	if (node == NULL)
+		return (1);
+	if (node->n <= min || node->n >= max)
+		return (0);
+	return (is_bst(node->left, min, node->n) &&
+		   is_bst(node->right, node->n, max));
 }
-
-int binary_tree_is_avl(const binary_tree_t *tree) {
-    if (tree == NULL) {
-        return 0;
-    }
-    if (!binary_tree_is_bst_helper(tree, INT_MIN, INT_MAX)) {
-        return 0;
-    }
-    return is_valid_avl_helper(tree) ? 1 : 0;
+/**
+ * binary_tree_is_avl - checks if a tree is an avl
+ * @tree: pointer to root of tree
+ * Return: 1 if its avl else 0
+*/
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (!is_bst(tree, INT_MIN, INT_MAX))
+	{
+		return (0);
+	}
+	return (valid_avl(tree) ? 1 : 0);
 }
